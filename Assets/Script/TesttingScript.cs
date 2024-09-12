d3b2290245b0bd187e4568b07f8903d4b8d87fd9
thanhnx <thanhnx.tkh@gmail.com>
2024-09-12 10:35:32 +0700

    + 24 - Scenes Management in Unity3d

--- Assets/Script/TesttingScript.cs
+++ Assets/Script/TesttingScript.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TesttingScript : MonoBehaviour
 {
-    private void Update()
-    {
-        Debug.Log("Update" + Time.deltaTime);
+    private void Start() {
+        Invoke(nameof(ChangeScenes),3f);
     }
-    private void FixedUpdate()
-    {
-        Debug.Log("Fixupdate" + Time.deltaTime);
+    private void ChangeScenes(){
+        SceneManager.LoadScene("Scene2");
     }
-    private void OnEnable() {
-        Debug.Log("OnEnable");
-        Invoke(nameof(OnDisable),3f);
-    }
+    // private void Update()
+    // {
+    //     Debug.Log("Update" + Time.deltaTime);
+    // }
+    // private void FixedUpdate()
+    // {
+    //     Debug.Log("Fixupdate" + Time.deltaTime);
+    // }
+    // private void OnEnable() {
+    //     Debug.Log("OnEnable");
+    //     Invoke(nameof(OnDisable),3f);
+    // }
 
-    private void OnDisable() {
-    Debug.Log("OnDisable");
+    // private void OnDisable() {
+    // Debug.Log("OnDisable");
 
-    }
+    // }
 }
