c6d61370f05f58978318a2ef91caba520786796f
thanhnx <thanhnx.tkh@gmail.com>
2024-09-12 15:50:16 +0700

    + Rotate Camera with Mouse in Unity 3D

---
+++ Assets/Script/FirstPersonCam.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class FirstPersonCam : MonoBehaviour {
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update () {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
