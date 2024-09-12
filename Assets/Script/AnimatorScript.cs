44509d5e322c33fca0c4fa90d6c9b65d8ff1cf21
thanhnx <thanhnx.tkh@gmail.com>
2024-09-12 08:57:46 +0700

    + 15 - Introduction to Animation Parameters in Unity

--- Assets/Script/AnimatorScript.cs
+++ Assets/Script/AnimatorScript.cs
@@ -7,8 +7,8 @@ public class AnimatorScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Cube").GetComponent<Animator>().speed =100;
-        GameObject.Find("Sphere").GetComponent<Animator>().speed =100;
+        GameObject.Find("Cube").GetComponent<Animator>().SetBool("IsMove",true);
+        GameObject.Find("Sphere").GetComponent<Animator>().SetBool("IsMove",true);
     }
 
     // Update is called once per frame
