c2ea38efedbb2ce6962cfed26d3b876ba54b042c
thanhnx <thanhnx.tkh@gmail.com>
2024-09-12 10:56:52 +0700

    + 26 - How to Detect Mouse Click or Touch on a GameObject

--- Assets/Script/TestScriptScenes.cs
+++ Assets/Script/TestScriptScenes.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestScriptScenes : MonoBehaviour
 {
@@ -13,6 +14,12 @@ public class TestScriptScenes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit)){
+            if(hit.transform.name == "Sphere"){
+                SceneManager.LoadScene("Scene2");
+            }
+        } 
     }
 }
--- Assets/Script/TestScriptScenes2.cs
+++ Assets/Script/TestScriptScenes2.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestScriptScenes2 : MonoBehaviour
 {
@@ -13,6 +14,12 @@ public class TestScriptScenes2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit)){
+            if(hit.transform.name == "Cube"){
+                SceneManager.LoadScene("Scene1");
+            }
+        } 
     }
 }
