49f5c44f6c832ada7b5f233f10e320ab40a925db
thanhnx <thanhnx.tkh@gmail.com>
2024-09-13 09:45:18 +0700

    + How to Prevent Mouse Click from Passing Through GUI Controls in Unity 3D

---
+++ Assets/Script/How to Prevent Mouse Click from Passing Through GUI Controls in Unity 3D/TapScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapScript : MonoBehaviour
+{
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)){
+                if(!IsPointerOverUIObject()){
+                    Debug.Log("Cube");
+                }
+            }
+        }
+    }
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+}
