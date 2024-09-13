5aaf56ad6f7d4ef1b81833eb456bc891c68816a4
thanhnx <thanhnx.tkh@gmail.com>
2024-09-13 10:08:47 +0700

    + Loading and Playing AudioClip at Runtime in Unity 3D

---
+++ Assets/Script/Loading and Playing AudioClip at Runtime in Unity 3D/AudioScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    AudioSource audioSource;
+    MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Resources.Load<AudioClip>("time_for_adventure");
+        meshRenderer.material = Resources.Load<Material>("Earth Material");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
